618a5645187c77434b38c921abe4554b39de1988
neon443 <69979447+neon443@users.noreply.github.com>
2025-08-09 08:44:47 -0400

    Initial Commit

---
+++ Loops/LoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopsApp.swift
+//  Loops
+//
+//  Created by neon443 on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
